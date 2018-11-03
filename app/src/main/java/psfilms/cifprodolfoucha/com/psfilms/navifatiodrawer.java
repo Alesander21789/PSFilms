a0f63797201fe7069736bc61a1f61b9b4e62a90e
Alesander <sanderfene21789@gmail.com>
2018-11-03 01:35:00 +0100

    boton

--- app/src/main/java/psfilms/cifprodolfoucha/com/psfilms/navifatiodrawer.java
+++ app/src/main/java/psfilms/cifprodolfoucha/com/psfilms/navifatiodrawer.java
@@ -1,5 +1,6 @@
 package psfilms.cifprodolfoucha.com.psfilms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class navifatiodrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    TableLayout table;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,74 @@ public class navifatiodrawer extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        table=(TableLayout)findViewById(R.id.tableLayout);
+
+        table.setStretchAllColumns(true);
+
+        TableRow fila = new TableRow(this);
+        //fila.setId();
+        TextView vista = new TextView(this);
+        vista.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista.setText("imagen 1");
+
+        TextView vista2 = new TextView(this);
+        vista2.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista2.setText("imagen 2");
+        TextView vista3 = new TextView(this);
+        vista3.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista3.setText("imagen 3");
+        TableRow fila2 = new TableRow(this);
+        //fila.setId();
+        TextView vista10 = new TextView(this);
+        vista10.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista10.setText("imagen 4");
+        TextView vista11 = new TextView(this);
+        vista11.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista11.setText("imagen 5");
+        TextView vista13 = new TextView(this);
+        vista13.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista13.setText("imagen 6");
+        TableRow fila3 = new TableRow(this);
+        //fila.setId();
+        TextView vista20 = new TextView(this);
+        vista20.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista20.setText("imagen 4");
+        TextView vista21 = new TextView(this);
+        vista21.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista21.setText("imagen 5");
+        TextView vista23 = new TextView(this);
+        vista23.setOnClickListener(_OnClickListener);
+        //vista.setId("");
+        vista23.setText("imagen 6");
+
+        fila.addView(vista);
+        fila.addView(vista2);
+        fila.addView(vista3);
+        fila2.addView(vista10);
+        fila2.addView(vista11);
+        fila2.addView(vista13);
+        fila3.addView(vista20);
+        fila3.addView(vista21);
+        fila3.addView(vista23);
+        table.addView(fila);
+        table.addView(fila2);
+        table.addView(fila3);
+
+
+
+
+
     }
 
     @Override
@@ -67,11 +139,46 @@ public class navifatiodrawer extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.buscar) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.inicio_imagen_registro:
+
+
+                Intent intent = new Intent(this, registro.class);
+                //EditText editText = (EditText) findViewById(R.id.editText);
+                //String message = editText.getText().toString();
+                // intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+                // Toast toast1 = Toast.makeText(getApplicationContext(), "Prueba ASF", Toast.LENGTH_SHORT);
+
+
+                // toast1.show();
+
+                return true;
+
+
+            case R.id.inicio_imagen_login:
+
+
+                Intent intent2 = new Intent(this, LoggedActivity.class);
+                //EditText editText = (EditText) findViewById(R.id.editText);
+                //String message = editText.getText().toString();
+                // intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent2);
+
+                // Toast toast1 = Toast.makeText(getApplicationContext(), "Prueba ASF", Toast.LENGTH_SHORT);
+
+
+                // toast1.show();
+
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -94,4 +201,19 @@ public class navifatiodrawer extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    private View.OnClickListener _OnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(navifatiodrawer.this, informacion.class);
+            //EditText editText = (EditText) findViewById(R.id.editText);
+            //String message = editText.getText().toString();
+            // intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+
+        }
+    };
 }
